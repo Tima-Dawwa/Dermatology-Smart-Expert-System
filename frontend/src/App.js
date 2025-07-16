619a422a31cdd6a047425c62a727d4b0b308beb7
TimaDawaa <tima302t@gmail.com>
2025-07-16 20:15:41 +0300

    react js frontend

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./App.css";
+import DermatologyExpertSystem from "./dermatology";
+
+function App() {
+  return (
+    <div className="App">
+      <DermatologyExpertSystem />
+    </div>
+  );
+}
+
+export default App;
